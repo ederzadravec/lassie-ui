265fc70bbfaed7f0f7f32a0f3b3a48eb9a1c7dfb
Eder Zadravec <ederzadravec@gmail.com>
2020-04-04 00:50:53 -0300

    add icone no botão

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import RNIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { Text } from '../';
 
@@ -12,6 +13,8 @@ const Container = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
   border-width: 1;
+
+  flex-direction: row;
 
   ${({ variant, theme }) => {
     if (variant === 'outline')
@@ -37,6 +40,10 @@ const Label = styled.Text`
   font-size: 14;
   font-weight: bold;
 
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+
   ${({ variant, color, theme }) => {
     if (variant === 'outline' || variant === 'transparent')
       return `
@@ -45,14 +52,45 @@ const Label = styled.Text`
   }}
 `;
 
-export const Button = ({ children, variant, color: colorProp, disabled, onPress }) => {
+const ButtonIcon = styled(RNIcon)`
+  font-size: 30;
+  color: ${({ theme }) => theme.palette.colors.white};
+
+  ${({ margin, theme }) => `margin-${margin}: ${theme.spacing.unit}`};
+
+  ${({ variant, color, theme }) => {
+    if (variant === 'outline' || variant === 'transparent')
+      return `
+        color: ${theme.palette[color].main};
+      `;
+  }}
+`;
+
+export const Button = ({
+  children,
+  variant,
+  color: colorProp,
+  disabled,
+  onPress,
+  iconBefore,
+  iconAfter,
+  ...props
+}) => {
   const color = disabled ? 'disabled' : colorProp;
 
   return (
-    <Container variant={variant} color={color} disabled={disabled} onPress={onPress}>
+    <Container {...props} variant={variant} color={color} disabled={disabled} onPress={onPress}>
+      {iconBefore && (
+        <ButtonIcon name={iconBefore} variant={variant} color={color} margin="right" />
+      )}
+
       <Label as={Text} variant={variant} color={color}>
         {children}
       </Label>
+
+      {iconAfter && (
+        <ButtonIcon name={iconAfter} variant={variant} color={color} margin="left" />
+      )}
     </Container>
   );
 };
@@ -69,4 +107,6 @@ Button.proptypes = {
   color: PropTypes.oneOf(['primary', 'error', 'alert', 'success']),
   onPress: PropTypes.func,
   disabled: PropTypes.bool,
+  iconBefore: PropTypes.string,
+  iconAfter: PropTypes.string,
 };
